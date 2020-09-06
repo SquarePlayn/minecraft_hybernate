c9308faa870152dc01892b9c3673bd728677d0d9
Square Play'n <pietervoors@hotmail.com>
2020-09-07 01:41:22 +0200

    Methods for reading different data types and read handshake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+from typing import Tuple
 
 # START Settings
 
@@ -10,16 +11,44 @@ LISTEN_PORT = 25565
 # END Settings
 
 def dprint(*args, **kwargs):
-    """Print a debug message when DEBUG = True"""
+    """
+    Print a debug message when DEBUG = True
+    """
     if DEBUG:
         print(*args, **kwargs)
 
 
-def read_var_int(sock: socket.socket) -> int:
-    return -100
+def read_var_int(sock: socket.socket) -> Tuple[int, int]:
+    """
+    Read a VarInt from a socket stream
+    """
+    result = 0
+    for bytes_read in range(5):
+        read = ord(sock.recv(1))
+        value = read & 0b01111111
+        result |= (value << (7 * bytes_read))
+
+        if read & 0b10000000 == 0:
+            return result, bytes_read + 1
+
+    raise Exception("More than 5 bytes in VarInt")
 
 
-def handle_client_socket(client_socket: socket.socket, client_address: tuple) -> None:
+def read_string(sock: socket.socket) -> Tuple[str, int]:
+    """
+    Read a string from a socket stream
+    """
+    length, num_read = read_var_int(sock)
+    read = sock.recv(length)
+    result = read.decode("utf-8")
+    return result, num_read
+
+
+def read_unsigned_short(sock: socket.socket) -> Tuple[int, int]:
+    return ord(sock.recv(1)) * (2 ** 8) + ord(sock.recv(1)), 2
+
+
+def handle_client_socket(client_socket: socket.socket, client_address: Tuple[str, int]) -> None:
     """
     Handle receiving a package.
 
@@ -28,15 +57,40 @@ def handle_client_socket(client_socket: socket.socket, client_address: tuple) ->
     """
     print(f"Received a packet from {client_address[0]}:{client_address[1]}")
 
-    # Read the first byte
+    # Read length and packet_id (present on any packet)
     length = read_var_int(client_socket)
     print(f"Length: {length}")
     packet_id = read_var_int(client_socket)
     print(f"Packet ID: {packet_id}")
 
+    if packet_id[0] == 0:
+        # Assuming it's a handshake, read fields
+        protocol_version = read_var_int(client_socket)
+        print(f"Protocol version: {protocol_version}")
+        server_address = read_string(client_socket)
+        print(f"Server address: {server_address}")
+        server_port = read_unsigned_short(client_socket)
+        print(f"Server port: {server_port}")
+        next_state = read_var_int(client_socket)
+        print(f"Next state: {next_state}")
+        leftover_int = read_var_int(client_socket)
+        print(f"Leftover int: {leftover_int}")
+
+        # Reply with the server status
+        send_server_status(client_socket, client_address)
+    else:
+        # Unknown packet
+        first_int = read_var_int(client_socket)
+        print(f"First int: ", first_int)
+
     # Close the connection
     client_socket.shutdown(True)
     client_socket.close()
+
+
+def send_server_status(client_socket: socket.socket, client_address: Tuple[str, int]) -> None:
+    # TODO
+    pass
 
 
 def main():
